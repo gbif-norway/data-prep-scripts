be770c5c43db48696de717347c48515eabf3c598
Michal Torma <michal.torma@nhm.uio.no>
2022-04-01 16:26:50 +0200

    Finish name and date digestion
    Co-authored-by: Rukaya <rukayaj@gmail.com>

--- scripts/fossil-db/helpers.py
+++ scripts/fossil-db/helpers.py
@@ -1,11 +1,12 @@
 # %%
 import datetime
+import string
 import pandas as pd
 from dateutil.parser import parse
 import re
 import logging
 # %%
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 def extract_names_and_dates(source: pd.Series):
@@ -18,13 +19,20 @@ def extract_name(source: str):
     src_name = []
     source_list = source.split(',')
     for item in source_list:
-        if _is_date(item):
-            logging.debug(f'Parsing {item} as a date')
-        else:
+        if _is_name(item):
             logging.debug(f'Parsing {item} as a name')
             src_name.append(_digest_name(item))
-    return pd.Series(src_name).dropna()
+    return [x for x in src_name if x != None]
 
+def _is_name(item: str): # [a-zæøå]
+    # check if only one word
+    item = item.strip()
+    if len(re.findall(pattern=r'^([A-Za-zæøå\.]+)$', string=item)) == 1:
+        return True
+    match_no = len(re.findall(pattern=r'([A-Za-zæøå]+)\.?[\s]*', string=item))
+    if match_no > 1:
+        return True
+    return False
 
 def extract_date(source: str):
     src_date = []
@@ -33,10 +41,13 @@ def extract_date(source: str):
         if _is_date(item):
             logging.debug(f'Parsing {item} as a date')
             src_date.append(_digest_date(item))
-        else:
-            logging.debug(f'Parsing {item} as a name')
-    return pd.Series(src_date).dropna()
-
+    src_date = [x for x in src_date if x != None]
+    if len(src_date) == 1:
+        return src_date[0]
+    if len(src_date) > 1:
+        return src_date[-1]
+        #raise Exception(f'Multiple dates detected: {source}')
+    return None
 
 def _is_date(candidate: str):
     numbers = re.findall(pattern="[0-9]", string=candidate)
@@ -44,8 +55,17 @@ def _is_date(candidate: str):
         return True
     return False
 
-
 def _digest_date(candidate: str):
+    # This is for candidates with a month and a year but no day
+    if len(re.findall(pattern=r'\w{3,}\.?\s+[0-9]{4}', string=candidate)) == 1:
+        spoofed_candidate = f'1. {candidate}'
+        try:
+            date = parse(spoofed_candidate, fuzzy=True)
+            return date.strftime('%Y-%m')
+        except Exception as e:
+            logging.warning(f'Unable to parse {candidate}\n {e}')
+
+    # Otherwise, just look for numbers to parse as a year
     num_of_digits = len(re.findall(pattern=r'[0-9]', string=candidate))
     if num_of_digits <= 4:
         match = re.findall(pattern=r'[0-9]{4}', string=candidate)
@@ -54,13 +74,13 @@ def _digest_date(candidate: str):
             return match[0]
 
     try:
-        date = parse(candidate, fuzzy=True)
+        date = parse(candidate, fuzzy=True, dayfirst=True)
         return date.strftime('%Y-%m-%d')
     except:
         return None
 
 
 def _digest_name(candidate: str):
-    if len(re.findall(pattern=r'[a-z]', string=candidate)) <= 2:
+    if len(re.findall(pattern=r'[A-Za-zæåø]', string=candidate)) <= 2:
         return None
     return candidate.strip()
