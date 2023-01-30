e2302c73840fc79dc6558ee9a31ac5e537257c6e
Michal Torma <michal.torma@nhm.uio.no>
2023-01-30 16:48:41 +0100

    Initial

---
+++ scripts/bendik-species-plots/script.py
@@ -0,0 +1,8 @@
+# %%
+import pandas as pd
+# %%
+orig_species = pd.read_excel('data/Species&Variables_RAW.xlsx', sheet_name='species')
+orig_vars = pd.read_excel('data/Species&Variables_RAW.xlsx', sheet_name='vars')
+# %%
+data
+# %%
