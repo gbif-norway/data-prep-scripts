d9cac3cec38fe3198d761e6ab20ec04a6463249f
rukayaj@gmail.com <rukayaj@gmail.com>
2023-01-31 10:32:06 +0100

    Refactor

--- scripts/uio-rocks/script.py
+++ scripts/uio-rocks/script.py
@@ -1,8 +1,8 @@
-#%%
 import pandas as pd
 from dateutil import parser
 import utm
-#%%
+#from pyproj import Proj
+
 df = pd.read_csv('Rock type_ Table.txt', sep=';', dtype='str', quotechar='"', encoding='utf-8')
 
 mapping = {
@@ -33,61 +33,35 @@ mapping = {
 }
 df.rename(columns=mapping, inplace=True)
 
-class MyParserInfo(parser.parserinfo):
-    MONTHS = [('Jan', 'January', 'januar'), 
-            ('Feb', 'February', 'februar'),
-            ('Mar', 'March', 'mars'),
-            ('Apr', 'April', 'april'),
-            ('May', 'May', 'mai'),
-            ('Jun', 'June', 'juni'),
-            ('Jul', 'July', 'juli'),
-            ('Aug', 'August'),
-            ('Sep', 'Sept', 'September'),
-            ('Oct', 'October', 'oktober'),
-            ('Nov', 'November'),
-            ('Dec', 'December')]
+df['eventDate'] = df['verbatimEventDate']
+date_fixes = ['-00-00', '-00', '00/00/', '00/', '00.00', '00.', '?', ' ']
+for date_fix in date_fixes:
+    df['eventDate'] = df['eventDate'].str.replace(date_fix, '')
+df.loc[df['eventDate'] == '0', 'eventDate'] = ''
+df = df.fillna('')
+date_fixes = {'`': '19', '': }
 
-def get_date_string(date):
-    if date is None or date == 0 or date == '0' or pd.isnull(date) or 'ukjent' in date.lower():
-        return None
-    date_fixes = [' - 1874-1879', 'xx/', 'xx/yy', '-00-00', '-00', '00/00/', '00/', '00.00', '00.', '?', ' ', '00:00:00', 'Summer', 'Sommeren', ',Oslo', 'Benecke', 'Vorrended.', 'J.Lehmann', 'Rosenbusch', ' - 1914-1915', ' - 1876-1877', ' - 1874-1879', ' - 1911-1912', 'HoelsSpitsbergeneksp.']
-    for date_fix in date_fixes:
-        date = date.replace(date_fix, '')
-    date = date.replace('`', '19')
-    date = date.strip(' .-/|')
-
+def parse_dates(date):
     try:
-        norwegian_months = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'oktober', 'feby']
-        for month in norwegian_months:
-           date = date.replace(month, month[:3]).replace('mai', 'may').replace('okt', 'oct')
-        period = pd.Period(date)
-        format = '%Y-%m-%d'
-        if period.freqstr == 'M':
-            format = '%Y-%m'
-        elif period.freqstr == 'A-DEC':
-            format = '%Y'
-        return period.strftime(format)
-    except pd._libs.tslibs.parsing.DateParseError as e:
-        try:
-            output = parser.parse(date, dayfirst=True, parserinfo=MyParserInfo())
-            return output.strftime('%Y-%m-%d')
-        except parser._parser.ParserError as e:
-            print(f'{e} - {date}')
+        new = parser.parse(date, )
+        if pd.isnull(new):
             return ''
+
+        if len(date) ==
+        return new
     except Exception as e:
-        print(f'{e} - {date}')
-        #import pdb; pdb.set_trace()
+        #print(e)
+        #print(date)
         return ''
 
-df['eventDate'] = df['verbatimEventDate'].apply(get_date_string)
-df['modified'] = df['modified'].apply(get_date_string)
-#df = df.fillna(' ')  # see https://github.com/pandas-dev/pandas/issues/11953
+df['eventDate'] = df['eventDate'].apply(parse_dates)
+df['modified'] = df['modified'].apply(parse_dates)
+df = df.fillna(' ')  # see https://github.com/pandas-dev/pandas/issues/11953
 
 def convert_utm(row):
     if  row['verbatimLatitude'] == '' or row['verbatimLatitude'] == '0' \
         or row['verbatimLongitude'] == '' or row['verbatimLongitude'] == '0' \
         or row['verbatimCoordinateSystem'] == '' or row['verbatimCoordinateSystem'] == '0' \
-        or pd.isnull(row['verbatimLatitude']) or pd.isnull(row['verbatimLongitude']) or pd.isnull(row['verbatimCoordinateSystem']) \
         or len(row['verbatimCoordinateSystem']) != 3:
         return ('', '')
     try:
@@ -96,19 +70,14 @@ def convert_utm(row):
         utmlat = int(row['verbatimLatitude'])
         utmlong = int(row['verbatimLongitude'])
         lat, long = utm.to_latlon(utmlong, utmlat, zone, zone_letter)
-        return (round(lat, 3), round(long, 3))
+        return (lat, long)
     except Exception as e:
-        return (None, None)
+        import pdb; pdb.set_trace()
 
 df['decimalDegrees'] = df.apply(convert_utm, axis=1)
 df['decimalLatitude'] = df['decimalDegrees'].str[0]
 df['decimalLongitude'] = df['decimalDegrees'].str[1]
 
-df['catalogNumber'] = df['catalogNumber'].str.strip(' ,|/-')
-df.dropna(subset='catalogNumber', inplace=True)
-df['catalogNumber'] = 'NHMO-MU-' + df['catalogNumber']
-print('Duplicates:')
-print(df.loc[df.duplicated('catalogNumber'), 'catalogNumber'])
+df['catalogNumber'] = 'NHMO-MU-' + df['catalogNumber'].str.rstrip(', ')
 
-df.to_csv('occurrence.txt', sep='\t', index=False)
-import pdb; pdb.set_trace()
+df.to_csv('occurrence.csv', sep='\t')
